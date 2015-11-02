b800866a9a5438218176df6cd871f4fbfcb9e802
Kendra Ash & Shannon Mulloy & Lindsay Culver <student@epicodus.com>
2015-11-02 09:52:37 -0800

    add passing unit test for sending a message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,22 @@
 class Message < ActiveRecord::Base
   validates_presence_of :to, :from, :body
+  before_create :send_message
 
+  private
+    def send_message
+      begin
+        response = RestClient::Request.new(
+          :method => :post,
+          :url => 'https://api.twilio.com/2010-04-01/Accounts/AC4aaa4850e14a8a60494913179ecd44bd/Messages.json',
+          :user => 'AC4aaa4850e14a8a60494913179ecd44bd',
+          :password => '06439abc95cd81f97adc0c4a56ca2118',
+          :payload => { :Body => body,
+                        :To => to,
+                        :From => from
+                        }
+        ).execute
+    rescue
+      false
+    end
+  end
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -5,4 +5,9 @@ describe Message do
   it { should validate_presence_of :from }
   it { should validate_presence_of :body }
 
+  it "doesn't save the message if twilio gives an error" do
+    message = Message.new(:body => 'hi', :to => '1111111', :from => '5039463641')
+    message.save.should be false
+  end
+  
 end
